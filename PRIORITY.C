592439cad054cf0f1ffb7a9363882686ac82c872
prakashjha18 <41507803+prakashjha18@users.noreply.github.com>
2018-08-08 21:22:52 +0530

    implementation of priority queue using structure

--- PRIORITY.C
+++ PRIORITY.C
@@ -1,87 +1,102 @@
 #include<stdio.h>
 #include<conio.h>
-#define max 5
-int arr[max],front=-1,rear=-1;
+# define SIZE 5
+int arr[SIZE],front=-1,rear=-1;
+struct prior
+{
+int data;
+int priority;
+}s[SIZE];
 void enqueue()
 {
-int ele,i,j,temp;
-printf("enter the element");
-scanf("%d",&ele);
-if(rear==max-1 && front==0)
-printf("overflow");
+int i,j,k=0;
+printf("enter the element\n");
+scanf("%d",&s[k].data);
+printf("enter the priority\n");
+scanf("%d",&s[k].priority);
+if(rear==SIZE-1)
+{
+	printf("overflow");
+}
 else
 {
- if(front==-1)
- {front=0;rear=0;}
- else
- rear++;
- arr[rear]=ele;
-}
-for(i=front;i<=rear;i++)
-{
- for(j=front;j<=rear;j++)
- {
-  if(arr[i]<arr[j])
-  {
-   temp=arr[i];
-   arr[i]=arr[j];
-   arr[j]=temp;
-   }
- }
-}
+if(rear==-1)
+	{
+		rear=0;
+		front=0;
+	}
+else
+	{
+	  rear++;
+	  }
+	  arr[rear]=s[k].data;
+	}
+
 }
 void dequeue()
 {
-int i,j,temp;
-for(i=front;i<=rear;i++)
-{
- for(j=front;j<=rear;j++)
- {
-  if(arr[i]<arr[j])
-  {
-   temp=arr[i];
-   arr[i]=arr[j];
-   arr[j]=temp;
-   }
- }
-}
 if(front==-1)
+{
 printf("underflow");
+}
 else
 {
- printf("deleted = %d",arr[front]);
- if(front==rear)
- {front=-1;rear=-1;}
- else
- {
- front++;
- }
+printf("deleted=%d",arr[front]);
+}
+if(front==rear)
+{
+front=-1;
+rear=-1;
+}
+else
+{
+front++;
 }
 }
 void display()
 {
-int i;
+int i,j,temp;
 for(i=front;i<=rear;i++)
 {
-printf("%d,",arr[i]);
+for(j=front+1;j<=rear;j++)
+{
+if(s[i].priority>s[j].priority)
+{
+ temp=arr[i];
+ arr[i]=arr[j];
+ arr[j]=temp;
 }
+}
+}
+
+	printf("the priority queue is\n");
+	for(i=front;i<=rear;i++)
+	 {
+	    printf("%d ",arr[i]);
+	 }
 }
 void main()
 {
-int i;
-clrscr();
-printf("1)enter\n2) delete\n3)display\n4)exit\n");
-do
-{
-printf("\nenter the number");
-scanf("%d",&i);
-switch(i)
-{
-case 1: enqueue(); break;
-case 2: dequeue(); break;
-case 3: display(); break;
+	int ch;
+	clrscr();
+	do
+	 {
+	   printf("\n1:Enqueue\n2:Dequeue\n3:Display\n4:Exit\n");
+	   printf("enter your choice\n");
+	   scanf("%d",&ch);
+	 switch(ch)
+	 {
+	   case 1:enqueue();
+	   break;
+	   case 2:dequeue();
+	   break;
+	   case 3:display();
+	   break;
+	   case 4:printf("exit");
+	   break;
+	 }
+	 }
+	 while(ch<=3);
 
-}
-}while(i<=3);
-getch();
-}
+	getch();
+	}
